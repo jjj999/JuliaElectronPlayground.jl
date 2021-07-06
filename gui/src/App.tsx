5fcbfeac1f3e46b2814165f1bbbe90854520ff9d
jjj999 <spaceship2021@gmail.com>
2021-07-06 10:20:15 +0900

    Changed the coordinate of the left box.

--- gui/src/App.tsx
+++ gui/src/App.tsx
@@ -29,7 +29,7 @@ export function App() {
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
       <Box position={[-1.2, 0, 0]} />
-      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
     </Canvas>
   );
 }
